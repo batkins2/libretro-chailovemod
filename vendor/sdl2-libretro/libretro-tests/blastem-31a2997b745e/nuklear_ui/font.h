9d0b8bc4bf6b86b73dc67d03bbfaf3e31095dc37
Brian Atkins <batkins2@gmail.com>
2024-09-15 21:38:59 -0400

    prep sdl2-libretro mod

--- vendor/sdl2-libretro/libretro-tests/blastem-31a2997b745e/nuklear_ui/font.h
+++
@@ -1,6 +0,0 @@
-#ifndef FONT_H_
-#define FONT_H_
-
-char *default_font_path(void);
-
-#endif //FONT_H_
--- vendor/sdl2-libretro/libretro-tests/test_sdlgfx/softmouse.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOFTMOUSE_H__
-#define SOFTMOUSE_H__
-
-extern void DBGcreatmousecursor();
-extern void DBGfreemousecursor();
-extern void DBGshowmouse(int active);
-
-#endif
