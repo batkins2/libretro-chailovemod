983a5522479eaacdd5f25b06d57c0b56b5b599d1
Brian Atkins <batkins2@gmail.com>
2024-10-01 20:37:45 -0400

    wip

--- src/love/chai_gfx.cpp
+++ src/love/chai_gfx.cpp
@@ -2,15 +2,16 @@
 
 namespace love
 {
+
 namespace gfx
 {
 
 Shader *wrap_newShader(const char *FileName) {
-    if (instance::graphics::isCreated()) {
-
+    if (instance()->isCreated()) {
+        File *file = filesystemmod::Filesystem::openFile(FileName, filesystemmod::File::Mode::MODE_READ);
+        Shader::CompileOptions *options;
+        return instance()->newShader(file->read(), options);
     }
-
-
 }
 
 }
--- src/love/chai_gfx.h
+++ src/love/chai_gfx.h
@@ -1,10 +1,17 @@
-#pragma once
-
 #include "gfx.h"
 #include "shader.h"
+#include "common/Module.h"
+#include "filesystem/Filesystem.h"
+
 
 namespace love
 {
+
+class Module;
+class Filesystemmod;
+class File;
+class Shader;
+
 namespace gfx
 {
     love::gfx::graphics *instance = (Module::getInstance<love::gfx::graphics>(Module::M_GRAPHICS));
